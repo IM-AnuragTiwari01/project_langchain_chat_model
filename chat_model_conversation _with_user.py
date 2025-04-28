0f0228239a9b476a0efd22766f986809b8734a98
Anurag Tiwari <anurag.tiwari.en@gmail.com>
2025-04-28 22:17:06 +0530

    v2 : handling chat history to keep going with the conversation with chat model but after the model will stop the history will be deleted  so in next version we'll use the fire base to store chat_history in cloud

--- chat_model_conversation _with_user.py
+++ chat_model_conversation _with_user.py
@@ -1,17 +1,54 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
 load_dotenv()
 
 
-messages = [
-    SystemMessage(content = "Solve the following maths problem"),
-    HumanMessage(content = "what is the square root of 49"),
-]
+# messages = [
+#     SystemMessage(content = "Solve the following maths problem"),
+#     HumanMessage(content = "what is the square root of 49"),
+# ]
+
 
 # --------Google chat model
 
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
-result = llm.invoke(messages)
-print(f"Answer from model is : {result.content}")
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+chat_history = [] # use this to store prev messages
+
+system_message = SystemMessage(content = "You are a helpful ai assistant")
+
+chat_history.append(system_message) #added the system_message to the history
+
+
+#chat loop
+#chat can be exited by typing exit and enter
+
+while True:
+    query = input("You: ")
+    if query.lower()=="exit":
+        break # exit the conversation
+    chat_history.append(HumanMessage(content = query))
+
+    result = model.invoke(chat_history)
+    response = result.content
+    chat_history.append(AIMessage(content = response))
+
+    print(f"AI response: {response}")
+
+print("-------Message History------------")
+print(chat_history)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
